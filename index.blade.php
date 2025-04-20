5f3767349c475a66e18d016c3cd9c1d0b851e799
Dzikr <07.dzikri@gmail.com>
2025-04-20 20:05:25 +0700

    lima

---
+++ index.blade.php
@@ -0,0 +1,11 @@
+<!-- lirik lagu beat beat
+
+[Verse 1: Kyujin, Lily, Jiwoo, Sullyoon, *Haewon*]
+Saekdareun simjangsori jjarithae, yeah, yes, y'all (Uh, uh)
+Yes, y’all, yes, y'all (NMIXX)
+Seollemi teojil deuthae like a shooting star
+Oh, rush (Oh, rush), oh, rush
+Up, asphalt wiro (Ooh) keun pajangi ilgo (Ooh)
+Naranhi here we go, so tell me what you're waiting for? (Ooh, ooh)
+*romaentikan new beat* (Ooh), *morachyeo, just blow your mind* (Your mind)
+*sumeul meomchugo feel the pulse* (Ooh, ooh) -->
