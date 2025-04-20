746836107946d91475ef8445145796be78dac043
Dzikr <07.dzikri@gmail.com>
2025-04-20 19:57:18 +0700

    satu

---
+++ latihan.php
@@ -0,0 +1,29 @@
+<?php
+    // echo 'hello world'.PHP_EOL;
+    // echo 'nama saya';
+    // var_dump (123.25);
+
+    $angka;
+
+
+    $angka = 142000;
+    echo $angka;
+
+
+    $angka = 5;
+    if ($angka > 0){
+        echo $angka.'postif';
+    } else {
+        echo '$angka negatif';
+    }
+
+
+    echo PHP_EOL;
+
+    $bilangan = [1,2,3,4,5];
+    $bil = array(5,4,3,2,1);
+    echo $bilangan[2].PHP_EOL;
+    echo $bil[2].PHP_EOL;
+
+    echo 'hello world'.PHP_EOL;
+    echo 'nama saya';
