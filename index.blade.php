0a7739651914df4bed0e2ad6f799a906f5fc4c10
Dzikr <07.dzikri@gmail.com>
2025-04-20 20:05:58 +0700

    enam

--- index.blade.php
+++ index.blade.php
@@ -9,3 +9,11 @@ Up, asphalt wiro (Ooh) keun pajangi ilgo (Ooh)
 Naranhi here we go, so tell me what you're waiting for? (Ooh, ooh)
 *romaentikan new beat* (Ooh), *morachyeo, just blow your mind* (Your mind)
 *sumeul meomchugo feel the pulse* (Ooh, ooh) -->
+
+
+[Pre-Chorus: Bae, Jiwoo]
+Da-ri-dam, da-ri-dam, simjangsorin drum
+Da-ri-dam, da-ri-dam, tteugeowojin pulse
+Da-ri-dam, da-ri-dam, be my rock and roll
+Tikitaka jakku ullyeodaeneun mami
+Dugeundugeun neoreul umjigiji my beat
